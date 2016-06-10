8397f1fde3a482b1990989c4f631df391f313e08
Ian McCowan <ian.mccowan@nuance.com>
2016-06-10 08:00:19 -0700

    vimfx

---
+++ vimfx/config.js
@@ -0,0 +1,5 @@
+vimfx.set('mode.normal.tab_select_previous', 'K')
+vimfx.set('mode.normal.tab_select_next', 'J')
+
+vimfx.set('mode.normal.tab_move_backward', 'K')
+vimfx.set('mode.normal.tab_move_forward', 'J')
